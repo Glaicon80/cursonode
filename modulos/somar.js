dad15281d75167e85b82d12f13f412d870650493
Glaicon Alves <glaicon.florisbelo@gmail.com>
2024-01-01 11:48:07 -0300

    primeiro passo com node.js

---
+++ modulos/somar.js
@@ -0,0 +1,9 @@
+//para executar os codigos javascript dentro de um arquivo
+//basta vc digitar dentro da pasta do projeto o comando
+// node "nome do arquivo com a extensão"
+
+function soma(a,b){
+    return a + b
+}
+
+console.log(soma(2,4))
